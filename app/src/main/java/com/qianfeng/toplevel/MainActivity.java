2a990ae55ba7667c33be2bff3ceb6ec5ed1ddac0
wuhao <805256509@qq.com>
2016-06-27 11:32:52 +0800

    New changelist

---
+++ app/src/main/java/com/qianfeng/toplevel/MainActivity.java
@@ -0,0 +1,18 @@
+package com.qianfeng.toplevel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+          mTextView = (TextView) findViewById(R.id.tv_main);
+    }
+}
